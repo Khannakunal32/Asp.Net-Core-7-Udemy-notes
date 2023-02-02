ed28a87ea1da2e3bfe5afc218c46fa3b4edfa372
Khannakunal32 <khannakunal32@gmail.com>
2023-02-02 09:48:14 +0530

    anonymous and labda functions

---
+++ Extra C# Essentials/AnonymousFunctions/AnonymousFunctions/Program.cs
@@ -0,0 +1,39 @@
+﻿using ClassLibrary1;
+
+namespace AnonymousFunctions
+{
+
+    // anonymous funcions are name-less methods that can be called with deligate variable
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            // create obj of class1 where delegate is defined
+            Class1 delegateExample = new Class1();
+
+            // creating event delegate event in main function namelessly
+            // syntax 
+            //EventName += delegate(param1, param2,....){
+
+            //}
+
+            delegateExample.myEvent += delegate (int a, int b)
+            {
+                Console.WriteLine(a + ", " + b);
+            };
+
+
+            // createing anonymous even using lambda expression
+            delegateExample.myEvent += (int a, int b) =>
+            {
+                Console.WriteLine(a + b);
+            };
+
+            // calling back the method to execute using raise event
+            delegateExample.RaiseEvent(10, 20);
+
+        }
+    }
+}
---
+++ Extra C# Essentials/AnonymousFunctions/ClassLibrary1/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace ClassLibrary1
+{
+    // creating delegate type event that can be called without any name 
+    public delegate void MydelegateType(int a, int b);
+
+    public class Class1
+    {
+
+        // creating envent
+        public event MydelegateType myEvent;
+
+        public void RaiseEvent(int a, int b)
+        {
+
+            // raise event
+            if (this.myEvent != null)
+            {
+                this.myEvent(a, b);
+            }
+        }
+
+    }
+}
+
+// Note for lambda expression
+// same as delegate but we dont have to use delegate keyword
+
+// Note for delegate
+// anonymous methods can be used anywhere within the method, to create Instantly without defining a method at class level
+// Advantage: we need not create a named method to quickly handle an event
+// it cannot be called without a delegate or event
+// it cannot contain jump statements like go to break or continue
+// it can access local variable and parameter of outer methods 
+// it is maninly used for event handler
+// it can't access ref or out parameter of an outer method 
